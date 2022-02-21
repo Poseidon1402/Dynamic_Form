f10f1892a762fa4eed202d9b8a2b1c2090544f97
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-02-21 22:38:02 +0300

    A minor changes

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,6 +13,11 @@ class ApplicationController extends AbstractController
     #[Route('/', name: 'subscribe_path', methods: ['GET', 'POST'])]
     public function index(): Response
     {
-        return $this->render('application/index.html.twig');
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class)
+            ->add('sector', EntityType::class)
+            ->add('course', EntityType::class)
+        ;
+        return $this->render('application/index.html.twig', compact('form'));
     }
 }
