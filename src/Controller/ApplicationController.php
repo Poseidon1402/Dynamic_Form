2209e4e405d904039171bf58ccde6f8687d92d53
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-02-21 22:45:21 +0300

    create student entity and migrate it into the database

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,11 +11,6 @@ class ApplicationController extends AbstractController
     #[Route('/', name: 'subscribe_path', methods: ['GET', 'POST'])]
     public function index(): Response
     {
-        $form = $this->createFormBuilder()
-            ->add('name', TextType::class)
-            ->add('sector', EntityType::class)
-            ->add('course', EntityType::class)
-        ;
         return $this->render('application/index.html.twig', compact('form'));
     }
 }
---
+++ src/Entity/Student.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudentRepository::class)]
+#[ORM\Table(name: 'Students')]
+class Student
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $sector;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $course;
+
+    #[ORM\Column(type: 'date')]
+    private $birthDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSector(): ?string
+    {
+        return $this->sector;
+    }
+
+    public function setSector(string $sector): self
+    {
+        $this->sector = $sector;
+
+        return $this;
+    }
+
+    public function getCourse(): ?string
+    {
+        return $this->course;
+    }
+
+    public function setCourse(string $course): self
+    {
+        $this->course = $course;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeInterface
+    {
+        return $this->birthDate;
+    }
+
+    public function setBirthDate(\DateTimeInterface $birthDate): self
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+}
