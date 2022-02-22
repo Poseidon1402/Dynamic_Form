87d734f1d52b74e88da42c4345976dde5157388e
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-02-22 17:23:54 +0300

    storing users'input into the database

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Student;
 use App\Entity\Students;
 use App\Form\StudentType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,13 +14,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApplicationController extends AbstractController
 {
     #[Route('/', name: 'subscribe_path', methods: ['GET', 'POST'])]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
         $student = new Students;
 
         $form = $this->createForm(StudentType::class, $student);
 
         $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $em->persist($student);
+            $em->flush();
+        }
 
         return $this->render('application/index.html.twig', [
             'form' => $form->createView()
