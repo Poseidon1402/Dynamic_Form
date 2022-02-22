b5dded7489dca423e53c64d80b19442f7d976068
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-02-22 16:07:49 +0300

    configure Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,10 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+import 'bootstrap';
+
+// or get all of the named exports for further usage
+import * as bootstrap from 'bootstrap';
+
 // start the Stimulus application
 import './bootstrap';
