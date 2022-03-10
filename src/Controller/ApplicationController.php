68132f408758db3df27b8879fa4cdfa2d1c99e76
RAJOELISON Aina Tiavina <rajoelisonainatiavina@gmail.com>
2022-03-10 22:50:02 +0300

    listen to the pre_set_data event and linked all course which have a relation with the sector

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Sector;
 use App\Entity\Student;
 use App\Entity\Students;
 use App\Form\StudentType;
+use App\Repository\SectorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,10 +16,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApplicationController extends AbstractController
 {
     #[Route('/', name: 'subscribe_path', methods: ['GET', 'POST'])]
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(Request $request, EntityManagerInterface $em, SectorRepository $rep): Response
     {
         $student = new Students;
+        $sector = $rep->findBy(['name' => 'sciences'])[0];
 
+        $student->setSector($sector);
+        
         $form = $this->createForm(StudentType::class, $student);
 
         $form->handleRequest($request);
--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -5,17 +5,23 @@ namespace App\Form;
 use App\Entity\Course;
 use App\Entity\Sector;
 use App\Entity\Students;
+use App\Repository\CourseRepository;
+use App\Repository\SectorRepository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class StudentType extends AbstractType
 {
+    public function  __construct(private CourseRepository $courseRepository){}
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -38,6 +44,7 @@ class StudentType extends AbstractType
                     ])
                 ]
             ])
+            /*
             ->add('course', EntityType::class, [
                 'class' => Course::class,
                 'choice_label' => 'name',
@@ -49,10 +56,31 @@ class StudentType extends AbstractType
                     ])
                 ]
             ])
+            */
             ->add('birthDate', DateType::class, [
                 'widget' => 'single_text'
             ])
         ;
+        
+        //listen to the event PRE_SET_DATA
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event){
+            $sector = $event->getData()->getSector() ?? null;
+            
+            //retrieve all course which is wrapped with a sector
+            $course = $this->courseRepository->createQueryBuilder('c')
+                ->andWhere('c.sector = :val')
+                ->setParameter('val', $sector)
+                ->orderBy('c.name', 'ASC')
+                ->getQuery()
+                ->getResult()
+            ;
+            
+            $event->getForm()->add('course', EntityType::class, [
+                'class' => Course::class,
+                'choice_label' => 'name',
+                'choices' => $course
+            ]);
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
